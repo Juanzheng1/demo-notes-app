2a09897701766730eb186ffad54fc444d586f617
juan <juanzheng0624@gmail.com>
2023-02-24 00:00:45 +1100

    Setting up our React app

---
+++ frontend/src/Routes.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Home from "./containers/Home";
+import NotFound from "./containers/NotFound";
+export default function Links() {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      {
+     /* Finally, catch all unmatched routes */
+     }
+    <Route path="*" element={<NotFound />} />;
+    </Routes>
+  );
+}
